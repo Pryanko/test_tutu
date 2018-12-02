c243a1c9b153176810c5c4c57746f57efdb36cc2
Greg Pryamov <pryamov.g@gmail.com>
2018-12-02 16:41:51 +0300

    #Feature Launcher
    - Добавление DI scopes

---
+++ app/src/main/kotlin/ru/tutu/stations/di/FragmentScope.java
@@ -0,0 +1,13 @@
+package ru.tutu.stations.di;
+
+import javax.inject.Scope;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Grigoriy Pryamov
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FragmentScope {
+}
