f4ed6d2919e7092d3ec7111d6c21b57cc35f7b6d
Greg Pryamov <pryamov.g@gmail.com>
2018-12-04 22:38:03 +0300

    #Feature LocalDb Module
    - Создание ключевых классов для Station

--- localdb/src/main/kotlin/ru/tutu/stations/localdb/AppDatabase.kt
+++ localdb/src/main/kotlin/ru/tutu/stations/localdb/AppDatabase.kt
@@ -2,18 +2,24 @@ package ru.tutu.stations.localdb
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import ru.tutu.stations.localdb.room.CountryDao
+import ru.tutu.stations.localdb.room.dao.CountryDao
+import ru.tutu.stations.localdb.room.dao.StationDao
 import ru.tutu.stations.localdb.room.entity.CountryEntity
+import ru.tutu.stations.localdb.room.entity.StationEntity
 
 /**
  * @author Grigoriy Pryamov.
  */
 @Database(
     version = BuildConfig.DB_VERSION, exportSchema = false,
-    entities = [CountryEntity::class
+    entities = [
+        CountryEntity::class,
+        StationEntity::class
     ]
 )
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun countryDao(): CountryDao
+
+    abstract fun stationDao(): StationDao
 }
